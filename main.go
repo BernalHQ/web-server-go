54e974f2e0902b014f2ce27a4c9086ab7e8ae4dd
BernalHQ <bernalhq19@gmail.com>
2024-04-12 22:52:41 -0600

    Step 2, return html file

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		tmlp, err := template.ParseFiles("index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmlp.Execute(w, nil)
+
+	default:
+		fmt.Fprint(w, "Unsupperted method:", r.Method)
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "Hello world!!!")
-	})
+	http.HandleFunc("/", handleRequest)
 
 	http.ListenAndServe(":80", nil)
 }
